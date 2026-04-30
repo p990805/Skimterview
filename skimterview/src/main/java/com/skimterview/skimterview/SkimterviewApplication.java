a1c5affdd1ca0c2ea8cf82389981402c73682b22
박주찬 <p990805@naver.com>
2026-04-30 16:17:29 +0900

    feat: 오타로 인한 폴더 재생성

---
+++ skimterview/src/main/java/com/skimterview/skimterview/SkimterviewApplication.java
@@ -0,0 +1,13 @@
+package com.skimterview.skimterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkimterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkimterviewApplication.class, args);
+	}
+
+}
---
+++ skimterview/src/test/java/com/skimterview/skimterview/SkimterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skimterview.skimterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkimterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
